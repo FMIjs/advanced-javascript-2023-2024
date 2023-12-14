eea0534734de8cf9258e96e96fc10cafdb628086
FIZLIN <akostov94@gmail.com>
2023-12-14 11:22:58 +0200

    Solution for ex#7

---
+++ week9/exercise/solution/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const users = ["Gosho", "Pesho", "Ivan"];
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.post("/user", (req, res) => {
+  const index = req.body.index;
+  const newUserName = req.body.username;
+  if (index) {
+    users[index] = newUserName;
+  } else {
+    users.push(newUserName);
+  }
+  res.redirect("/");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/", (req, res) => {
+  res.render("main", { message: "TESTING...", users });
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening on :8080");
+});
