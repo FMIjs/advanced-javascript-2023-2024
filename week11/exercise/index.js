79b19b52c023f985e97cb2509024b372207e43a0
FIZLIN <akostov94@gmail.com>
2024-01-11 10:07:49 +0200

    ex#11

--- week11/exercise/index.js
+++ week11/exercise/index.js
@@ -15,20 +15,28 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
+app.use('/api', require('./api'));
+
+
+const messages = [
+  { userName: "John", message: "Hello, I am John" }
+];
 io.on("connection", (socket) => {
   connectedSockets = connectedSockets.concat(socket);
 
-  setInterval(() => {
-    socket.emit("system-message", "Hello from server");
-  }, 2000);
+  socket.emit('message-history', messages);
 
-  socket.on("chat-message", (message) => {
-    io.emit("chat-message", message);
+  socket.on("chat-message", (messageData) => {
+    messages.push(messageData);
+    io.emit("chat-message", messageData);
   });
 
   socket.on("disconnected", () => {
     connectedSockets = connectedSockets.filter(s => s !== socket);
   });
+  setInterval(() => {
+    socket.emit("system-message", "Hello from server");
+  }, 2000);
 });
 
 
